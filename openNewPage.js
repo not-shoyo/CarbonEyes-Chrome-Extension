42b63935629af6d02970b6160c27fb2a8e866b8b
not-shoyo <akheelsaajid@gmail.com>
2023-02-11 13:40:54 +0530

    commit to pull

---
+++ openNewPage.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  document.getElementById("link").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    console.log("clicked");
+
+    //linking to externally hosted website
+    // window.open("https://www.nitk.ac.in/", "_blank");
+
+    // linking to local file
+    window.open("index.html", "_blank");
+  });
+};
