da20dc25573e1526744323a60315d4ab706683e0
not-shoyo <akheelsaajid@gmail.com>
2023-02-12 03:20:08 +0530

    table done

--- extensionWebsite.js
+++ extensionWebsite.js
@@ -1,5 +1,6 @@
 let tableData = new Map();
 let existingDomains = new Set();
+let existingTabs = new Set();
 
 // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //   if (request.message) {
@@ -85,6 +86,7 @@ function refreshData() {
         domain.set(tab, { size, emission: findEmission(size), cumulativeEmission: findEmission(size), url });
       }
     });
+
     const tableBody = document.getElementById("tableBody");
 
     console.log(tableData);
@@ -93,45 +95,115 @@ function refreshData() {
       if (!existingDomains.has(domain)) {
         existingDomains.add(domain);
 
-        const tableRow = document.createElement("tr");
-        const rowNum = document.createElement("td");
-        const rowDomainName = document.createElement("td");
-        const rowSize = document.createElement("td");
-        const rowEmission = document.createElement("td");
+        const tableRow = document.createElement("div");
+        const rowBtn = document.createElement("button");
+        const rowNum = document.createElement("span");
+        const rowDomainName = document.createElement("span");
+        const rowSize = document.createElement("span");
+        const rowEmission = document.createElement("span");
+
+        tableRow.className = "custom-row";
+        rowBtn.className = "custom-accordion-button";
+        rowBtn.value = domain;
+
+        const rowPanel = document.createElement("div");
+        rowPanel.className = "custom-toggle-able-panel";
+        rowPanel.id = domain;
 
         let totalSize = 0;
         let totalEmission = 0;
 
-        tabsMapping.forEach((record, tabs) => {
+        tabsMapping.forEach((record, tab) => {
+          existingTabs.add(tab);
           totalSize += record.size;
           totalEmission += record.cumulativeEmission;
+
+          const subRow = document.createElement("div");
+          const subRowUrl = document.createElement("span");
+          const subRowSize = document.createElement("span");
+          const subRowEmission = document.createElement("span");
+
+          subRow.className = "custom-row";
+          subRowUrl.className = "sub-row-url";
+          subRowSize.className = "sub-row-size";
+
+          subRowUrl.innerText = record.url;
+          subRowSize.innerText = record.size;
+          subRowEmission.innerText = record.cumulativeEmission;
+
+          subRow.appendChild(document.createElement("span"));
+          subRow.appendChild(document.createElement("span"));
+          subRow.appendChild(subRowUrl);
+          subRow.appendChild(subRowSize);
+          subRow.appendChild(subRowEmission);
+
+          rowPanel.appendChild(subRow);
         });
 
+        rowBtn.onclick = (e) => {
+          var panel = rowPanel;
+          if (panel.style.display === "flex") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "flex";
+          }
+        };
+
+        rowBtn.innerText = "Expand";
         rowNum.innerText = idx + 1;
         rowDomainName.innerText = domain;
         rowSize.innerText = totalSize;
-        // rowEmission.innerText = totalEmission;
         rowEmission.innerText = findEmission(totalSize);
 
+        tableRow.appendChild(rowBtn);
         tableRow.appendChild(rowNum);
         tableRow.appendChild(rowDomainName);
         tableRow.appendChild(rowSize);
         tableRow.appendChild(rowEmission);
         tableBody.appendChild(tableRow);
+        tableBody.appendChild(rowPanel);
       } else {
         const tableRow = tableBody.children[idx];
+
+        const rowPanel = document.getElementById(domain);
+        // rowPanel.innerHTML = "";
 
         let totalSize = 0;
         let totalEmission = 0;
 
-        tabsMapping.forEach((record, tabs) => {
+        tabsMapping.forEach((record, tab) => {
           totalSize += record.size;
           totalEmission += record.cumulativeEmission;
+
+          if (!existingTabs.has(tab)) {
+            existingTabs.add(tab);
+
+            const subRow = document.createElement("div");
+            const subRowUrl = document.createElement("span");
+            const subRowSize = document.createElement("span");
+            const subRowEmission = document.createElement("span");
+
+            subRow.className = "custom-row";
+            subRowUrl.className = "sub-row-url";
+            subRowSize.className = "sub-row-size";
+
+            subRowUrl.innerText = record.url;
+            subRowTabId.innerText = tab;
+            subRowSize.innerText = record.size;
+            subRowEmission.innerText = record.cumulativeEmission;
+
+            subRow.appendChild(document.createElement("span"));
+            subRow.appendChild(document.createElement("span"));
+            subRow.appendChild(subRowUrl);
+            subRow.appendChild(subRowSize);
+            subRow.appendChild(subRowEmission);
+
+            rowPanel.appendChild(subRow);
+          }
         });
 
-        tableRow.children[2].innerText = totalSize;
-        // tableRow.children[3].innerText = totalEmission;
-        tableRow.children[3].innerText = findEmission(totalSize);
+        tableRow.children[3].innerText = totalSize;
+        tableRow.children[4].innerText = findEmission(totalSize);
       }
     });
   });
@@ -142,3 +214,24 @@ setInterval(refreshData, 1000);
 // chrome.storage.local.get("data", (data)=>{
 // console.log(JSON.parse(data))
 //})
+
+// var buttons = document.getElementsByClassName("custom-accordion-button");
+// var allPanels = document.getElementsByClassName("custom-toggle-able-panel");
+
+// // console.log(buttons);
+// // console.log(allPanels);
+
+// var i;
+
+// for (i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", (e) => {
+//     var panel = document.getElementById(e.target.value);
+
+//     // console.log(panel);
+//     if (panel.style.display === "flex") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "flex";
+//     }
+//   });
+// }
