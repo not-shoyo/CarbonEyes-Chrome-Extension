5e6b7394d0e02e21ca758efbe2cd186d78c74d17
not-shoyo <akheelsaajid@gmail.com>
2023-02-11 16:40:20 +0530

    commit to merge

--- background.js
+++ background.js
@@ -1,17 +1,11 @@
-
-
-
-
 // Listen for messages from the background page
-    chrome.runtime.onMessage.addListener(
-       function(request, sender, sendResponse) {
-        if (request.greeting === "Hello from background page!") {
-            console.log("Received message: " + request.greeting);
-        }
-        });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.greeting === "Hello from background page!") {
+    console.log("Received message: " + request.greeting);
+  }
+});
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(JSON.parse(request.message));
-    localStorage.setItem("xcvxvc",request.message )
-    sendResponse({ message: "Hello from the background script!" });
-  });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(JSON.parse(request.message));
+  sendResponse({ message: "Hello from the background script!" });
+});
