be8c2cf04cb4e796637c7968622c642176040010
Ayman161803 <mohammedaymannawaz@gmail.com>
2023-02-11 18:52:57 +0530

    akheel is gae. v gae
    Signed-off-by: Ayman161803 <mohammedaymannawaz@gmail.com>

--- extensionWebsite.js
+++ extensionWebsite.js
@@ -1,5 +1,16 @@
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log(request.message);
-  document.getElementById("dataType").innerHTML = request.message
-});
+
+function refreshData() {
+chrome.storage.local.get("data", (data)=>{
+  if(data) console.log(data);
+  document.getElementById("dataType").innerHTML = JSON.stringify(data)
+})
+}
+
+setInterval(refreshData, 3000)
+
+
+
+// chrome.storage.local.get("data", (data)=>{
+// console.log(JSON.parse(data))
+//})
